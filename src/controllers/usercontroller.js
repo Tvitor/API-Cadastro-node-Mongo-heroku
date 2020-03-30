7714f614beca70919b789557716a34ba4ba6a0f8
tvitor <contato.vst@gmail.com>
2020-03-30 20:12:34 -0300

    test get

--- src/controllers/usercontroller.js
+++ src/controllers/usercontroller.js
@@ -2,6 +2,10 @@ const express = require("express");
 const router = express.Router();
 const userMethods = require("../models/user/usermethods.js");
 
+router.get('/test'),  (req, res) =>{
+    console.log('testou');
+}
+
 router.post('/register',  (req, res)=> {
     userMethods.userRegister(req, res)
     
