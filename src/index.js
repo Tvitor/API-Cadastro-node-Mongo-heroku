304a1045dbbc521fbebc48a7fa71a76ee0862b2f
tvitor <contato.vst@gmail.com>
2020-03-30 15:29:12 -0300

    start

--- src/index.js
+++ src/index.js
@@ -1,14 +1,13 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 require('dotenv-safe').config();
+const PORT = process.env.PORT || 3000;
 
-const app = express();
-
+const app = express()
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false}));
 
 require('./controllers/usercontroller')(app);
 require('./controllers/authcontroller')(app);
 
-const PORT = process.env.PORT || 3000;
 app.listen(PORT);
