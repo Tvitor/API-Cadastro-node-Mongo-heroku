4c3bfb19894aa66a8b08dbd395ebe3fac75cd5bc
tvitor <contato.vst@gmail.com>
2020-03-31 14:30:33 -0300

    retornando rotas

--- config/index.js
+++ config/index.js
@@ -4,12 +4,11 @@ const app = express()
 if(process.env.PORT !== 'production'){
     require('dotenv').config()
   }
-let host = '0.0.0.0';
+
 app.use(bodyParser.urlencoded({ extended: true}));
 app.use(bodyParser.json());
 
-app.get('/', (req, res)=> res.send('hello'))
-// require('../src/controllers/usercontroller')(app);
+ require('../src/controllers/usercontroller')(app);
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, host, () =>{console.log(`Our app is running on port ${ PORT }`);});
+app.listen(PORT, () =>{console.log(`Our app is running on port ${ PORT }`);});
