5de087f587bf995e61ab48d9f8343bd5a7c6dcbf
tvitor <contato.vst@gmail.com>
2020-03-31 04:11:32 -0300

    3000

--- config/index.js
+++ config/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000;
 
 const app = express()
 app.use(bodyParser.json());
