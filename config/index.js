1769af9177172cfb7c05ed62c109e27a936dfd8b
tvitor <contato.vst@gmail.com>
2020-03-31 14:27:34 -0300

    host

--- config/index.js
+++ config/index.js
@@ -4,7 +4,7 @@ const app = express()
 if(process.env.PORT !== 'production'){
     require('dotenv').config()
   }
-
+let host = '0.0.0.0';
 app.use(bodyParser.urlencoded({ extended: true}));
 app.use(bodyParser.json());
 
@@ -12,4 +12,4 @@ app.get('/', (req, res)=> res.send('hello'))
 // require('../src/controllers/usercontroller')(app);
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () =>{console.log(`Our app is running on port ${ PORT }`);});
+app.listen(PORT, host, () =>{console.log(`Our app is running on port ${ PORT }`);});
