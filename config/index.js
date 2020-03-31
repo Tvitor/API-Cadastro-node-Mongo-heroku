9296557bca29cf078437ee17c77ef56e375fbdbd
tvitor <contato.vst@gmail.com>
2020-03-31 04:23:07 -0300

    port

--- config/index.js
+++ config/index.js
@@ -1,12 +1,11 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-
-const PORT = process.env.PORT || 3000;
-
 const app = express()
+
+app.use(bodyParser.urlencoded({ extended: true}));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false}));
 
 require('../src/controllers/usercontroller')(app);
 
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () =>{console.log(`Our app is running on port ${ PORT }`);});
