c6dc26ca31b17be78631f6d15dfdd7ab77663e66
vitor <contato.vst@gmail.com>
2020-03-31 20:47:43 -0300

    alterando posicao index
    index

--- index.js
+++ index.js
@@ -9,8 +9,8 @@ const app = express()
 app.use(bodyParser.urlencoded({ extended: true}));
 app.use(bodyParser.json());
 
- require('../src/controllers/usercontroller')(app);
- require('../src/controllers/authcontroller')(app);
+ require('./src/controllers/usercontroller')(app);
+ require('./src/controllers/authcontroller')(app);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () =>{console.log(`Our app is running on port ${ PORT }`);});
