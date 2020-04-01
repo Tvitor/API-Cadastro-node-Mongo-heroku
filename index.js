fa34b980e6a7936937a4eb101fdb9b6d1a55777f
vitor <contato.vst@gmail.com>
2020-04-01 00:31:33 -0300

    port

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 
-if(process.env.PORT !== 'production'){
-  require('dotenv').config()
-}
+require('dotenv').config()
+
 const app = express()
+const PORT = process.env.PORT ? process.env.PORT : 3000;
 
 app.use(bodyParser.urlencoded({ extended: true}));
 app.use(bodyParser.json());
@@ -12,5 +12,4 @@ app.use(bodyParser.json());
  require('./src/controllers/usercontroller')(app);
  require('./src/controllers/authcontroller')(app);
 
-const PORT = process.env.PORT || 3000;
 app.listen(PORT, () =>{console.log(`Our app is running on port ${ PORT }`);});
